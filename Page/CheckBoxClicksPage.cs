77feb38bea88500998b43b5d5b0325a4312aca90
Andre <andre.gvazdauskaite@gmail.com>
2022-03-03 17:34:40 +0200

    CheckBoxTest With Page Object Pattern

---
+++ Page/CheckBoxClicksPage.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+
+namespace CheckBoxTest.Page
+{
+    public class CheckBoxClicksPage
+    {
+        private readonly IWebDriver Driver;
+
+        public IWebElement ButtonElement => Driver.FindElement(By.Id("check1"));
+        public IWebElement AgeTxtElement => Driver.FindElement(By.Id("isAgeSelected"));
+        public IWebElement AgeCheckboxElement => Driver.FindElement(By.Id("isAgeSelected"));
+        public IReadOnlyCollection<IWebElement> CheckboxElements => Driver.FindElements(By.CssSelector(".cb1-element"));
+
+        public CheckBoxClicksPage(IWebDriver Driver)
+        {
+            this.Driver = Driver;
+        }
+
+        public void ClickButton()
+        {
+            ButtonElement.Click();
+        }
+
+        public bool IsButtonWithValue(string Value)
+        {
+            return Value.Equals(ButtonElement.GetAttribute("value"));
+        }
+
+        public void ClickOnCheckbox(bool ShouldBeChecked)
+        {
+            if (AgeCheckboxElement.Selected != ShouldBeChecked)
+            {
+                AgeCheckboxElement.Click();
+            }
+        }
+
+        public void ClickAllCheckboxes(bool ShouldBeChecked)
+        {
+            foreach (IWebElement checkbox in CheckboxElements)
+            {
+                if (checkbox.Selected != ShouldBeChecked)
+                {
+                    checkbox.Click();
+                }
+            }
+        }
+    }
+}
---
+++ Test/CheckBoxClicksTest.cs
@@ -0,0 +1,61 @@
+﻿using CheckBoxTest.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CheckBoxTest.Test
+{
+    public class CheckBoxClicksTest
+    {
+        private static IWebDriver Driver;
+        private static CheckBoxClicksPage Page;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            Driver = new ChromeDriver();
+            Page = new CheckBoxClicksPage(Driver);
+            Driver.Url = "http://demo.seleniumeasy.com/basic-checkbox-demo.html";
+            Driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        { 
+            Driver.Quit();
+        }
+
+
+        [Test]
+        public static void Test1OneCheckbox()
+        {
+            Page.ClickOnCheckbox(true);
+            Assert.IsTrue("Success - Check box is checked".Equals(Page.AgeTxtElement.Text), $"Text is not the same, actual text is {Page.AgeTxtElement.Text}");
+        }
+
+        [Test]
+        public static void Test2ManyCheckboxes()
+        {
+            Page.ClickOnCheckbox(false);
+            Page.ClickAllCheckboxes(true);
+            Assert.IsTrue(Page.IsButtonWithValue( "Uncheck All"), "Text is not correct");
+        }
+
+        [Test]
+        public static void Test3ManyCheckboxs()
+        {
+            Page.ClickOnCheckbox(false);
+            if (!Page.IsButtonWithValue("Uncheck All"))
+            {
+                Page.ClickAllCheckboxes(true);
+            }
+            Page.ClickButton();
+
+            foreach (IWebElement checkbox in Page.CheckboxElements)
+            {
+                Assert.IsFalse(checkbox.Selected);
+            }
+            Assert.IsTrue(Page.IsButtonWithValue("Check All"), "Text is not correct");
+        }
+    }
+}
